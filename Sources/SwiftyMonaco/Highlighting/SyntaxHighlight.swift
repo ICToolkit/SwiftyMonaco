ce5bd5819c0fc93d2aa265a6dd1ba009df7e351f
Pavel Kasila <pavel.kasila@gmail.com>
2021-03-20 20:20:44 +0300

    Add test language

--- Sources/SwiftyMonaco/Highlighting/SyntaxHighlight.swift
+++ Sources/SwiftyMonaco/Highlighting/SyntaxHighlight.swift
@@ -24,4 +24,5 @@ public struct SyntaxHighlight {
 
 public extension SyntaxHighlight {
     static let swift = SyntaxHighlight(title: "Swift", fileURL: Bundle.module.url(forResource: "swift", withExtension: "json", subdirectory: "Languages")!)
+    static let test = SyntaxHighlight(title: "Test", fileURL: Bundle.module.url(forResource: "test", withExtension: "json", subdirectory: "Languages")!)
 }
